84691836df8c50d903d3b1599d71d372e74a7425
ClimbingAl <aristotlestarteditall@gmail.com>
2024-09-20 10:07:04 +0100

    Code tidying to add function composition $ and constants

--- hqdm/app/Main.hs
+++ hqdm/app/Main.hs
@@ -13,14 +13,20 @@ import qualified Data.ByteString.Lazy as BL
 import Data.Csv (HasHeader( NoHeader ), decode)
 -- import Data.List (sortUniq)
 import qualified Data.Vector as V
+import Data.String (String)
 -- import Data.Tree (Tree(subForest))
+
+hqdmInputFilename = "hqdmAllAsDataNoClassName.csv"
+thing = "hqdm:e5ec5d9e-afea-44f7-93c9-699cd5072d90"
+classOfSpatiotemporalextent = "hqdm:bb6f6d3f-1ed1-41ab-942c-6b3667c5da37"
 
 main :: IO ()
 main = do
     --file_contents <- readFile "hqdmAllAsDataNoParentheses.stmt"
      --putStrLn file_contents
 
-    hqdmTriples <- fmap V.toList . decode @HqdmInput NoHeader <$> BL.readFile "hqdmAllAsDataNoClassName.csv"
+
+    hqdmTriples <- fmap V.toList . decode @HqdmInput NoHeader <$> BL.readFile hqdmInputFilename
 
     let hqdmInputModel = either (const []) id hqdmTriples
     print hqdmInputModel
@@ -39,31 +45,28 @@ main = do
 
     -- Exercise functions to find the thing object's relations (triples) and then the type predicate for thing
     putStr "\nGet the entire Thing object type:\n\n"
-    let thing = lookupHqdmOne "hqdm:e5ec5d9e-afea-44f7-93c9-699cd5072d90" hqdmInputModel
-    let thingType = lookupHqdmType thing
+    let thingObj = lookupHqdmOne thing hqdmInputModel
+    let thingType = lookupHqdmType thingObj
     print (head thingType)
 
     -- Find the subtypes of a given thing
     putStr "\nGet the subtypes of given thing:\n\n"
     let subtypes = lookupSubtypes hqdmInputModel
-    -- class_of_spatiotemporalextent bb6f6d3f-1ed1-41ab-942c-6b3667c5da37
-    -- thing e5ec5d9e-afea-44f7-93c9-699cd5072d90
-    let findHqdmTypesInList = fmap (\x -> head (lookupHqdmType (lookupHqdmOne x hqdmInputModel)))
+    -- Use constants above for thing or classOfSpatiotemporalextent
+    let findHqdmTypesInList = fmap (\x -> head (lookupHqdmType $ lookupHqdmOne x hqdmInputModel))
 
-    let thingNodeSubtypes = lookupSubtypeOf "hqdm:bb6f6d3f-1ed1-41ab-942c-6b3667c5da37" subtypes
-    let thingSubtypes = findHqdmTypesInList thingNodeSubtypes 
+    let thingSubtypes = findHqdmTypesInList $ lookupSubtypeOf classOfSpatiotemporalextent subtypes
     print (thingSubtypes)
 
     -- Find the supertypes of a given thing
     putStr "\nGet the supertypes of given thing:\n\n"
-    let thingNodeSupertypes = lookupSupertypeOf "hqdm:bb6f6d3f-1ed1-41ab-942c-6b3667c5da37" subtypes
-    let thingSupertypes = findHqdmTypesInList thingNodeSupertypes 
+    let thingSupertypes = findHqdmTypesInList $ lookupSupertypeOf classOfSpatiotemporalextent subtypes 
     print (thingSupertypes)
 
     ---------------------------------------------
 
     let hqdmRawPredicates = getPredicates hqdmInputModel
-    let uniquePredicates = stringListSort ( uniqueIds hqdmRawPredicates )
+    let uniquePredicates = stringListSort $ uniqueIds hqdmRawPredicates 
 
     putStr "\n\nList of predicates:\n\n"
     print uniquePredicates
@@ -74,11 +77,11 @@ main = do
     -- Get Top Level Thing
 
     putStr "\n\nHow many thing nodes (not a very clever test as it is really just testing if the the string is in the unique list):\n\n"
-    let numThings = howmanyNodes (=="hqdm:e5ec5d9e-afea-44f7-93c9-699cd5072d90") uniqueNodes
-    print numThings
+    let numThings = howmanyNodes (==thing) uniqueNodes
+    print (numThings)
 
     ---------------------------------------------
-    -- Find the supertypes all the way to thing
+    -- Find the supertypes all the way to thing by recursion
 
     -- Take the result and compose a list of the relations inherited down the tree, via all paths
 
